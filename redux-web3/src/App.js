810e575fdac2add6bba16afccaf8de8c970124fe
khanoc <38735197+Muddi00seven@users.noreply.github.com>
2022-05-08 16:10:08 +0500

    web3 features added
    network add
    mobile wallet
    network switch
    account switch

--- redux-web3/src/App.js
+++ redux-web3/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from './redux/store'
-import { loadBlockchain } from './redux/slices/web3ConnectSlice'
+import { loadBlockchain, loadWalletConnect, updateAccount } from './redux/slices/web3ConnectSlice'
 import './App.css';
 
 function App() {
@@ -11,9 +11,15 @@ function App() {
 
   const dispatch = useAppDispatch()
   const { web3, accounts, contract } = useAppSelector((state) => state.web3Connect)
-  const handleWeb3Connect = () => {
+  const handleMetamask = () => {
     dispatch(loadBlockchain());
   }
+
+  const handleWalletConnect = () => {
+    dispatch(loadWalletConnect());
+  }
+
+
   console.log("states", amount, address)
 
   const balanceOf = async () => {
@@ -32,26 +38,62 @@ function App() {
       let transfer = await contract?.methods.transfer(address, value).send({
         from: accounts[0]
       })
-     await balanceOf()
-    console.log("transfer",transfer)
+      await balanceOf()
+      console.log("transfer", transfer)
     }
     catch (error) {
       console.log("error", error)
     }
   }
-
   useEffect(() => {
     console.log("balance function outside if")
     if (contract) {
       console.log("balance function is-side if")
       balanceOf()
     }
-  }, [contract])
+  }, [contract, accounts])
+
+  const switchNetwork = async () => {
+    try {
+      await web3.currentProvider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: "0x61" }]
+      })
+    }
+    catch (error) {
+      if (error.code == 4902) {
+        await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: '0x61',
+              chainName: "bsc testnet",
+              nativeCurrency: {
+                name: "bnb",
+                symbol: "bnb",
+                decimals: 18
+              },
+              blockExplorerUrls: [
+                "https://testnet.bscscan.com"
+              ],
+              rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"]
+            }
+          ]
+        })
+      }
+      console.log("error", error)
+    }
+  }
+
+  // account switch
+  window.ethereum.on('accountsChanged', async (data)=>{
+      dispatch(updateAccount(data))
+  })
   return (
     <div className="App">
       {userBalance
         ? <>
-          user Balance : {userBalance / 10**18}
+          user Balance : {userBalance / 10 ** 18}
         </>
         : ""
       }
@@ -64,11 +106,21 @@ function App() {
             <input value={address} onChange={(e) => setAddress(e.target.value)} placeholder='Address' />
             <br />
             <button onClick={() => transferErc20()}> Transfer</button>
+            <br /><br /><br />
+            <button onClick={() => switchNetwork()}>
+              Switch Network
+            </button>
           </>
           :
-          <button onClick={() => handleWeb3Connect()}>
-            Connect metamask
-          </button>
+          <>
+            <button onClick={() => handleMetamask()}>
+              Connect Metamask
+            </button>
+            <br /><br /><br />
+            <button onClick={() => handleWalletConnect()}>
+              Connect WalletConnect
+            </button>
+          </>
       }
 
     </div>
--- redux-web3/src/redux/slices/web3ConnectSlice.js
+++ redux-web3/src/redux/slices/web3ConnectSlice.js
@@ -1,6 +1,7 @@
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from '../../contract/contract';
 import Web3 from 'web3';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import WalletConnectProvider from '@walletconnect/web3-provider';
 
 export const initialState = {
     web3: null,
@@ -17,8 +18,9 @@ export const initialState = {
 export const loadBlockchain = createAsyncThunk("loadBlockchain", async (_, thunkAPI) => {
     try {
         //network should be rinkeby
-        console.log("Web3.givenProvider.chainId ",Web3.givenProvider.chainId )
-        if (Web3.givenProvider && Web3.givenProvider.chainId == "0x4") {
+        console.log("Web3.givenProvider.chainId ", Web3.givenProvider.chainId)
+        // if (Web3.givenProvider && Web3.givenProvider.chainId == "0x4") {
+        if (Web3.givenProvider) {
             await Web3.givenProvider.enable();
             const web3 = new Web3(Web3.givenProvider);
             console.log("web3", web3)
@@ -43,6 +45,55 @@ export const loadBlockchain = createAsyncThunk("loadBlockchain", async (_, thunk
 })
 
 
+
+export const loadWalletConnect = createAsyncThunk("loadWalletConnect", async (_, thunkAPI) => {
+    try {
+        
+        const provider =  new WalletConnectProvider({
+            rpc: {
+                4: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            },
+            chainId: 4,
+        })
+
+        if (provider) {
+            await provider.enable();
+            const web3 = new Web3(provider);
+            console.log("web3", web3)
+            const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+            const accounts = await web3.eth.getAccounts();
+            return {
+                web3,
+                accounts,
+                contract
+            }
+        }
+        else {
+            return {
+                web3LoadingErrorMessage: "error in connecting wallet"
+            }
+        }
+
+    }
+    catch (error) {
+        console.log("error", error)
+    }
+})
+
+
+export const updateAccount = createAsyncThunk("updateAccount", async (data, thunkAPI) => {
+    try {
+        let accounts =  data
+            return {
+                accounts,
+            }
+        }
+    
+    catch (error) {
+        console.log("error", error)
+    }
+})
+
 const web3ConnectSlice = createSlice({
     name: "Web3Connect",
     initialState,
@@ -54,6 +105,22 @@ const web3ConnectSlice = createSlice({
         ) => {
             state.web3 = payload?.web3;
             state.contract = payload?.contract;
+            state.accounts = payload?.accounts;
+
+        },
+        [loadWalletConnect.fulfilled.toString()]: (
+            state,
+            { payload }
+        ) => {
+            state.web3 = payload?.web3;
+            state.contract = payload?.contract;
+            state.accounts = payload?.accounts;
+
+        },
+        [updateAccount.fulfilled.toString()]: (
+            state,
+            { payload }
+        ) => {
             state.accounts = payload?.accounts;
 
         }
